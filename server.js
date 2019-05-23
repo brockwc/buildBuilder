36180aa64745e8d085da59a30f32645de421a340
Brock Whitbread <brock.whitbread@gmail.com>
2019-05-23 16:53:25 -0700

    renamed app.js, modified references, and required modules

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
+
+// require .env file
+require('dotenv').config();
+
+// require db
+require('./config/database');
+
+// require passport
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +28,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'kashdfjhlsajhdgf',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
