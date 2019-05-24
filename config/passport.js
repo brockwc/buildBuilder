6c0ac16271faf890e839d6a959762ec514fd0bff
Brock Whitbread <brock.whitbread@gmail.com>
2019-05-24 10:02:14 -0700

    includes serilializer methods for session tokens

--- config/passport.js
+++ config/passport.js
@@ -27,3 +27,13 @@ passport.use(new GoogleStrategy({
     })
   }
 ));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
